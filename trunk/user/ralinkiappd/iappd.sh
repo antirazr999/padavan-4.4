739ea2554cf4dc768a07634a061512775cc3051a
antirazr999 <soaringeagle@sina.com>
2025-04-17 02:51:58 +0800

    Update iappd.sh

--- trunk/user/ralinkiappd/iappd.sh
+++ trunk/user/ralinkiappd/iappd.sh
@@ -2,9 +2,9 @@
 
 
 start() {   if grep -q 'mt76x3_ap' /proc/modules ; then
-	    ralinkiappd -wi rai0 -d 0          &
-	    sysctl -wq net.ipv4.neigh.rai0.base_reachable_time_ms=10000
-	    sysctl -wq net.ipv4.neigh.rai0.delay_first_probe_time=1
+	    ralinkiappd -wi ra0 -d 0          &
+	    sysctl -wq net.ipv4.neigh.ra0.base_reachable_time_ms=10000
+	    sysctl -wq net.ipv4.neigh.ra0.delay_first_probe_time=1
             else
 	    if grep -q 'rai0' /proc/interrupts; then
 	    ralinkiappd -wi rai0 -d 0   &
@@ -22,6 +22,10 @@ start() {   if grep -q 'mt76x3_ap' /proc/modules ; then
 	    sysctl -wq net.ipv4.neigh.eth2.delay_first_probe_time=1
 	    sysctl -wq net.ipv4.neigh.ra0.base_reachable_time_ms=10000
 	    sysctl -wq net.ipv4.neigh.ra0.delay_first_probe_time=1
+	    sed -i "/^FtR0khId1/d; \$aFtR0khId1=$(cat /sys/class/net/rai0/address | tr -d ':')" /etc/Wireless/iNIC/iNIC_ap.dat
+	    sed -i '/^FtMdId1/d; $a\FtMdId1=bbbb' /etc/Wireless/iNIC/iNIC_ap.dat
+	    sed -i "/^FtR0khId1/d; \$aFtR0khId1=$(cat /sys/class/net/ra0/address | tr -d ':')" /etc/Wireless/RT2860/RT2860AP.dat
+	    sed -i '/^FtMdId1/d; $a\FtMdId1=aaaa' /etc/Wireless/RT2860/RT2860AP.dat
 	    iptables -A INPUT -i br0 -p tcp --dport 3517 -j ACCEPT
 	    iptables -A INPUT -i br0 -p udp --dport 3517 -j ACCEPT 
 }
