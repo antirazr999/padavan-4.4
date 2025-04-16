f713dc91f8c6d786c83fcce0eb53556f90f41fe7
antirazr999 <soaringeagle@sina.com>
2025-04-16 08:40:03 +0800

    Update adguardhome.sh

--- trunk/user/adguardhome/adguardhome.sh
+++ trunk/user/adguardhome/adguardhome.sh
@@ -64,94 +64,49 @@ getconfig(){
 bind_host: 0.0.0.0
 bind_port: 3030
 language: zh-cn
-beta_bind_port: 0
 auth_name: adguardhome
 auth_pass: adguardhome
 auth_attempts: 500
 block_auth_min: 1
-http_proxy: ""
 rlimit_nofile: 0
-debug_pprof: false
-web_session_ttl: 720
 dns:
-  bind_hosts:
-  - 0.0.0.0
+  bind_host: 0.0.0.0
   port: 5335
-  statistics_interval: 1
-  querylog_enabled: true
-  querylog_file_enabled: true
-  querylog_interval: 1
-  querylog_size_memory: 1000
-  anonymize_client_ip: false
   protection_enabled: true
+  filtering_enabled: true
   blocking_mode: nxdomain
-  blocking_ipv4: ""
-  blocking_ipv6: ""
   blocked_response_ttl: 10
-  parental_block_host: family-block.dns.adguard.com
-  safebrowsing_block_host: standard-block.dns.adguard.com
+  querylog_enabled: true
   ratelimit: 20
   ratelimit_whitelist: []
   refuse_any: true
-  upstream_dns:
-  - https://dns.alidns.com/dns-query
-  upstream_dns_file: /root/AdguardHome_Upstreams.txt
   bootstrap_dns:
-  - tls://223.5.5.5
+  - 223.5.5.5
   all_servers: true
-  fastest_addr: false
   allowed_clients: []
   disallowed_clients: []
-  blocked_hosts:
-  - version.bind
-  - id.server
-  - hostname.bind
-  cache_size: 0
-  cache_ttl_min: 0
-  cache_ttl_max: 60
-  bogus_nxdomain: []
-  aaaa_disabled: false
-  enable_dnssec: true
-  edns_client_subnet: true
-  max_goroutines: 300
-  ipset: []
-  filtering_enabled: true
-  filters_update_interval: 24
+  blocked_hosts: []
+  parental_sensitivity: 0
   parental_enabled: false
   safesearch_enabled: false
   safebrowsing_enabled: false
-  safebrowsing_cache_size: 1048576
-  safesearch_cache_size: 1048576
-  parental_cache_size: 1048576
-  cache_time: 30
-  rewrites:
-  - domain: 'translate.googleapis.com'
-    answer: hkg07s37-in-f14.1e100.net
-  blocked_services: []
-  local_domain_name: lan
-  resolve_clients: true
-  local_ptr_upstreams: []
+  resolveraddress: ""
+  upstream_dns:
+  - 223.5.5.5
 tls:
   enabled: false
   server_name: ""
   force_https: false
   port_https: 443
   port_dns_over_tls: 853
-  port_dns_over_quic: 784
-  port_dnscrypt: 0
-  dnscrypt_config_file: ""
-  allow_unencrypted_doh: false
-  strict_sni_check: false
   certificate_chain: ""
   private_key: ""
-  certificate_path: ""
-  private_key_path: ""
 filters:
 - enabled: true
   url: https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
   name: AdGuard DNS filter
   id: 1
-- enabled: false
+- enabled: true
   url: https://adaway.org/hosts.txt
   name: AdAway Default Blocklist
   id: 2
@@ -167,46 +122,26 @@ filters:
   url: https://easylist-downloads.adblockplus.org/easyprivacy.txt
   name: EasyList-Privacy
   id: 5
-- enabled: true
+- enabled: false
   url: https://www.i-dont-care-about-cookies.eu/abp/
   name: cookies
   id: 6
-- enabled: true
-  url: https://gitee.com/xinggsf/Adblock-Rule/raw/master/rule.txt
-  name: 乘风-广告规则
-  id: 7
-- enabled: true
-  url: https://gitee.com/xinggsf/Adblock-Rule/raw/master/mv.txt
-  name: 乘风-视频规则
-  id: 8
-whitelist_filters: []
-user_rules:
-- '||funshion.com^'
+user_rules: []
 dhcp:
   enabled: false
   interface_name: ""
   dhcpv4:
-    gateway_ip: ""
-    subnet_mask: ""
-    range_start: ""
-    range_end: ""
-    lease_duration: 86400
-    icmp_timeout_msec: 1000
-    options: []
-  dhcpv6:
-    range_start: ""
-    lease_duration: 86400
-    ra_slaac_only: false
-    ra_allow_slaac: false
-clients: []
-log_compress: false
-log_localtime: false
-log_max_backups: 0
-log_max_size: 100
-log_max_age: 3
+  enabled: false
+  interface_name: ""
+  gateway_ip: ""
+  subnet_mask: ""
+  range_start: ""
+  range_end: ""
+  lease_duration: 86400
+  icmp_timeout_msec: 1000
 log_file: ""
-verbose: false
-schema_version: 10
+verbose: true
+schema_version: 3
 
 EEE
 	  chmod 755 "$adg_file"
