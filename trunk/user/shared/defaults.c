5eecd7fc942c64afbcfd40c0071bc992b36fa49e
antirazr999 <soaringeagle@sina.com>
2025-04-19 05:32:47 +0800

    Update defaults.c

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -350,8 +350,8 @@ struct nvram_pair router_defaults[] = {
 #endif
 	{ "rt_HT_AutoBA", "1" },
 	{ "rt_VgaClamp", "0" },
-	{ "rt_KickStaRssiLow", "-85" },
-	{ "rt_AssocReqRssiThres", "-85" },
+	{ "rt_KickStaRssiLow", "0" },
+	{ "rt_AssocReqRssiThres", "0" },
 
 	// guest AP 2.4GHz
 	{ "rt_guest_enable", "0" },
