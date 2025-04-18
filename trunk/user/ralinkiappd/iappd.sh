853935785cb6be4b05b19b206c410db63d788d9a
antirazr999 <soaringeagle@sina.com>
2025-04-19 03:51:49 +0800

    Update iappd.sh

--- trunk/user/ralinkiappd/iappd.sh
+++ trunk/user/ralinkiappd/iappd.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
 
-start() {   
-
+start() {
+		pid=`pidof ralinkiappd`
+	    if [ "$pid" != "" ]; then
+	        killall -q  ralinkiappd
+		sleep 1
+		killall -q  ralinkiappd
+		sleep 1
+		killall -q  ralinkiappd
+		sleep 1
+	    fi 
+     
 	    sysctl -wq net.ipv4.neigh.br0.base_reachable_time_ms=10000
 	    sysctl -wq net.ipv4.neigh.br0.delay_first_probe_time=1
 	    sysctl -wq net.ipv4.neigh.eth2.base_reachable_time_ms=10000
@@ -11,12 +20,15 @@ start() {
 	    sysctl -wq net.ipv4.neigh.ra0.delay_first_probe_time=1
 	    sysctl -wq net.ipv4.neigh.rai0.base_reachable_time_ms=10000
 	    sysctl -wq net.ipv4.neigh.rai0.delay_first_probe_time=1
+	    sysctl -wq net.ipv4.neigh.ra.base_reachable_time_ms=10000
+	    sysctl -wq net.ipv4.neigh.ra.delay_first_probe_time=1
 	    
 	    iptables -A INPUT -i br0 -p udp --dport 3517 -j ACCEPT
 	    iptables -A INPUT -i br0 -p tcp --dport 3517 -j ACCEPT
      
      	    ralinkiappd -wi rai0 -d 0 &
      	    ralinkiappd -wi ra0 -d 0 &
+     	    ralinkiappd -wi ra -d 0 &
 
 }
 
