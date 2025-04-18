11394fb27896aca521d60b174d4d4bb2311db2c6
antirazr999 <soaringeagle@sina.com>
2025-04-18 10:10:42 +0800

    Update iappd.sh

--- trunk/user/ralinkiappd/iappd.sh
+++ trunk/user/ralinkiappd/iappd.sh
@@ -2,12 +2,7 @@
 
 
 start() {   
- 	  sed -i "/^FtR0khId1/d; \$aFtR0khId1=$(cat /sys/class/net/rai0/address | tr -d ':')" /etc/Wireless/iNIC/iNIC_ap.dat
-     	  sed -i '/^FtMdId1/d; $a\FtMdId1=bb' /etc/Wireless/iNIC/iNIC_ap.dat
-     	  logger -t "更新KVR配置"
-     	  sed -i "/^FtR0khId1/d; \$aFtR0khId1=$(cat /sys/class/net/ra0/address | tr -d ':')" /etc/Wireless/RT2860/RT2860AP.dat
-     	  sed -i '/^FtMdId1/d; $a\FtMdId1=aa' /etc/Wireless/RT2860/RT2860AP.dat  
-	
+
 	    sysctl -wq net.ipv4.neigh.br0.base_reachable_time_ms=10000
 	    sysctl -wq net.ipv4.neigh.br0.delay_first_probe_time=1
 	    sysctl -wq net.ipv4.neigh.eth2.base_reachable_time_ms=10000
