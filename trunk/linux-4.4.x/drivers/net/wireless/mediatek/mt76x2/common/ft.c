75501953676082ce47ed4b94cacfb16d2852da05
antirazr999 <soaringeagle@sina.com>
2025-04-18 21:23:19 +0800

    Update ft.c

--- trunk/linux-4.4.x/drivers/net/wireless/mediatek/mt76x2/common/ft.c
+++ trunk/linux-4.4.x/drivers/net/wireless/mediatek/mt76x2/common/ft.c
@@ -2720,7 +2720,7 @@ void FT_rtmp_read_parameters_from_file(
 			if(simple_strtol(macptr, 0, 10) != 0)  /*Enable */
 				pAd->ApCfg.MBSSID[Loop].FtCfg.FtCapFlag.Dot11rFtEnable = TRUE;
 			else /*Disable */
-				pAd->ApCfg.MBSSID[Loop].FtCfg.FtCapFlag.Dot11rFtEnable = TRUE;
+				pAd->ApCfg.MBSSID[Loop].FtCfg.FtCapFlag.Dot11rFtEnable = FALSE;
 
 			DBGPRINT(RT_DEBUG_TRACE, ("I/F(ra%d) Dot11rFtEnable=%d\n",
 				Loop, pAd->ApCfg.MBSSID[Loop].FtCfg.FtCapFlag.Dot11rFtEnable));
@@ -2739,7 +2739,7 @@ void FT_rtmp_read_parameters_from_file(
 			if(simple_strtol(macptr, 0, 10) != 0)  /*Enable */
 				pAd->ApCfg.MBSSID[Loop].FtCfg.FtCapFlag.RsrReqCap = TRUE;
 			else /*Disable */
-				pAd->ApCfg.MBSSID[Loop].FtCfg.FtCapFlag.RsrReqCap = TRUE;
+				pAd->ApCfg.MBSSID[Loop].FtCfg.FtCapFlag.RsrReqCap = FALSE;
 
 			DBGPRINT(RT_DEBUG_TRACE, ("I/F(ra%d) Dot11rFtRic=%d\n",
 				Loop, pAd->ApCfg.MBSSID[Loop].FtCfg.FtCapFlag.RsrReqCap));
@@ -2756,7 +2756,7 @@ void FT_rtmp_read_parameters_from_file(
 				break;
 
 			if(simple_strtol(macptr, 0, 10) != 0)  /*Enable */
-				pAd->ApCfg.MBSSID[Loop].FtCfg.FtCapFlag.FtOverDs = FALSE;
+				pAd->ApCfg.MBSSID[Loop].FtCfg.FtCapFlag.FtOverDs = TRUE;
 			else /*Disable */
 				pAd->ApCfg.MBSSID[Loop].FtCfg.FtCapFlag.FtOverDs = FALSE;
 
